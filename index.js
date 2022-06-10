b5f3c450d9d2124a5c355c76f1f8ab8d1a0a316a
Moringa <alinoorshukri8@gmail.com>
2022-06-10 08:46:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    //use spread operator
+    const newEmp = {...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key]= value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    //use spread operator
+    const newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
